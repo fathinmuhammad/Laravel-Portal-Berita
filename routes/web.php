b2b24861f8d7ca8521f29b5901bce53d673bb84c
fathinmuhammadfadhlullah <fathinmuhammadfadhlullah@gmail.com>
2022-07-11 04:31:05 +0700

    Project Laravel

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController; //load controller post
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/testing', function () {
+    return response('<h1>Testing oke lancar jaya</h1>');
+});
+
+Route::resource('post', PostController::class);
